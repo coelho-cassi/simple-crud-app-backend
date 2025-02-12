7ae3e4053d51b84849386db3830724bb756b222a
TJ <coelhocassi@gmail.com>
2025-02-12 17:54:23 -0500

    hide creds

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const mongoose = require('mongoose');
 const Product = require('./models/product.model.js');
 const productRoute = require('./routes/product.route.js');
+const creds = require('./creds.json');
 const app = express();
 
+
+const dbUsername = creds.username;
+const dbPassword = creds.password;
 
 //middleware
 app.use( express.json());
@@ -12,7 +16,7 @@ app.use(express.urlencoded({extended: false}));
 //routes
 app.use('/api/products', productRoute);
 
-mongoose.connect('mongodb+srv://coelhocassi:Dna5XNP4FpS0337h@backenddb.wxafe.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB')
+mongoose.connect('mongodb+srv://' + dbUsername + ':' + dbPassword + '@backenddb.wxafe.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB')
   .then(() => {
     console.log('Connected to the database!');
     app.listen(3000, () => {
